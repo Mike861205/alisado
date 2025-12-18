5b3c9e2e1baf0a1d35597ea22b57f432014c418f
Mike861205 <miguel.palomera1986@gmail.com>
2025-12-18 15:02:52 -0700

    Fix syntax error in Results.tsx

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -57,7 +57,7 @@ const Results = () => {
           </p>
         </motion.div>
 
-        {/* Gallery Grid */}}
+        {/* Gallery Grid */}
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 lg:gap-6">
           {results.map((result, index) => (
             <motion.div
